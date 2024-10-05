21445a555bf3009c6636e6249dba971a0f0df8b2
Sergioaero <116237665+Sergioaero1@users.noreply.github.com>
2024-10-05 05:45:30 -0300

    Upgrade
    Só footer mesmo.

--- dapp/src/app/page.js
+++ dapp/src/app/page.js
@@ -48,7 +48,7 @@ export default function Home() {
         </div>
         <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
           <p className="col-4 mb-0 text-body-secondary">
-            &copy; 2024 BetCandidate, Inc
+            &copy; 2024 BetCandidate, Sergioaero
           </p>
           <ul className="nav col-4 justify-content-end">
             <li className="nav-item"><a href="/" className="nav-link px-2 text-body-secondary">Home</a></li>
